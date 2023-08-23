7a4993e548103b26545455baef8c868f3e3c4b51
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-08-23 12:38:51 +0500

    If-else

---
+++ 4. If-else/homework.js
@@ -0,0 +1,35 @@
+// if ("0") {
+//     alert("Hello")
+// }
+//! works
+// let a = 5;
+// let b = 10;
+
+// let result;
+
+// // if (a + b < 4) {
+// //   result = "Below";
+// // } else result = "Over";
+
+// result = a + b < 4 ? "Below" : "Over";
+
+let message;
+
+if (login == "Employee") {
+  message = "Hello";
+} else if (login == "Director") {
+  message = "Greetings";
+} else if (login == "") {
+  message = "No login";
+} else {
+  message = "";
+}
+
+message =
+  login == "Employee"
+    ? "Hello"
+    : login == "Director"
+    ? "Greetings"
+    : login == ""
+    ? "No login"
+    : "";
---
+++ 4. If-else/index.js
@@ -0,0 +1,46 @@
+// IF else in js
+//! if
+// let age = 19;
+// if (age > 18) {
+//   console.log("Sizning yoshingiz katta!");
+// }
+//! if/else
+// let age = 15;
+// if (age > 18) {
+//   console.log("Sizning yoshingiz katta!");
+// } else {
+//   console.log("Sizning kirishingiz mumkin emas!");
+// }
+//! if/else if/else
+// let age = 32;
+
+// if (age < 18 && age > 0) {
+//   console.log("Yosh bolasiz");
+// } else if (age < 35 && age >= 18) {
+//   console.log("Yoshsiz");
+// } else if (age >= 35 && age < 100) {
+//   console.log("siz qarisiz");
+// } else if (age > 100) {
+//   console.log("siz haliham tirikmisiz!");
+// } else {
+//   console.log("Bunday yosh bo'lmaydi");
+// }
+//! Ternary operator
+// let age = 32;
+// // let a;
+
+// // if (age < 18) {
+// //   a = "bola";
+// // } else {
+// //   a = "yosh";
+// // }
+
+// let a = age < 18 ? "bola" : "yosh";
+
+// console.log(a);
+//! Nullish collascing
+// let value = null;
+
+// let _val = value ?? 5; // null & undefined
+
+// console.log(_val);
