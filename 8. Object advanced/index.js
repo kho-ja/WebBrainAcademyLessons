36f4e788021b0e5e3c991c7faaa8bb67014538e0
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-04 10:48:01 +0500

    Object advaced

---
+++ 8. Object advanced/index.js
@@ -0,0 +1,75 @@
+//* Garbage collection
+// let obj = {
+//   name: "John",
+//   age: 35,
+// };
+// obj = null;
+
+//* this
+// let person = {
+//   name: "Aziz",
+//   age: 35,
+//   //   info() {
+//   //     console.log(this.name);
+//   //   },
+// //   info: function () {
+// //     console.log(this.name);
+// //   },
+//     // info: () => {
+//     //   console.log(this);
+//     // },
+// };
+
+// let person2 = person;
+// person = null;
+
+// person2.info();
+
+//* Object destructuring
+// let car = {
+//   name: "Lamborghinie",
+//   year: 2000,
+//   info: {
+//     color: "red",
+//   },
+// };
+
+// let {
+//   name: lam,
+//   year,
+//   info: { color },
+// } = car;
+
+// console.log(lam, year, color);
+
+//* ...
+// let car = {
+//   name: "Lamborghinie",
+//   year: 2000,
+//   info: {
+//     color: "red",
+//   },
+// };
+// let car2 = {
+//   size: 32,
+//   name: "bugati",
+// };
+// let obj = { ...car, ...car2 };
+
+// console.log(obj);
+// ? optinal chaining
+
+// let car = {
+//   name: "Lamborghinie",
+//   year: 2000,
+//   //   info: {
+//   //     color: "red",
+//   //   },
+// };
+
+// console.log(car.info?.car);
+
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
