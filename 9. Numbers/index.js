e582664b19d840b0a1841f65ec5fb38d9586c576
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-06 11:30:13 +0500

    Numbers

---
+++ 9. Numbers/index.js
@@ -0,0 +1,82 @@
+// *Numbers
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// * Format
+// let milliard = 1_000_000_000;
+// console.log(milliard);
+
+// let milliard = 1e9;
+// console.log(milliard);
+
+// parseInt, parseFloat
+
+//* Xato
+// console.log(0.1 + 0.2);
+
+// *toFixed
+// let n = 1309.5654652;
+// n = n.toFixed(2);
+// console.log(n);
+
+//* toPrecision
+// let num = 16354.894613;
+// num = num.toPrecision(10);
+
+// console.log(num);
+
+// 2 => 10
+// let n = 1100;
+// n = parseInt(n, 2);
+// console.log(n);
+
+// 10 => 2
+// let num = 72;
+// num = num.toString(2);
+// console.log(num);
+
+// * isNaN
+// console.log(isNaN(NaN));
+// console.log(isNaN(123));
+// console.log(isNaN({}));
+// console.log(isNaN(["asdf"]));
+// console.log(isNaN("hhjgjs"));
+
+// Object.is(), ===
+// console.log(Object.is("5", 5));
+
+// Math
+// console.log(Math.PI);
+// console.log(Math.E);
+// function
+// console.log(Math.abs(-12));
+// sin cos, tan
+// console.log(Math.sin(Math.PI * 2));
+// round
+// console.log(Math.round(546.6546));
+// floor
+// console.log(Math.floor(2.999));
+// console.log(Math.floor(2.001));
+// ceil
+// console.log(Math.ceil(2.01));
+// console.log(Math.ceil(2.99));
+// trunc
+// console.log(Math.trunc(2.01));
+// console.log(Math.trunc(2.99));
+// pow
+// console.log(Math.pow(2, 9));
+// console.log(2 ** 9);
+// min max
+// console.log(Math.min(5, 7, 8, 9, 3, 1, 5, 8, 8));
+// console.log(Math.max(5, 7, 8, 9, 3, 1, 5, 8, 8));
+// sqrt
+// console.log(Math.sqrt(64));
+// cbrt
+// console.log(Math.cbrt(27));
+// sign
+// console.log(Math.sign(-456));
+// console.log(Math.sign(897));
+// console.log(Math.sign(0));
+
+// *random
+// console.log(Math.random());
