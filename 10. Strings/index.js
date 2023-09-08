4ac5db02ed0641485d1ae902253612b6605a4b1c
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-08 11:35:29 +0500

    Strings

---
+++ 10. Strings/index.js
@@ -0,0 +1,72 @@
+// * String
+
+// let str1 = "";
+// let str2 = "";
+// let str3 = `String`;
+
+// string
+// let str4 = new String("String");
+// console.log(str4 === str3);
+
+// Escape charecter
+
+// let str = 'Gulla Yashna \t "O\'zbekiston"!';
+// console.log(str);
+//         01234567890
+let str = "O'zbekiston kelajagi buyuk davlat!";
+// console.log(str[-3]);
+// console.log(str.at(-3));
+// console.log(str.charAt(3));
+
+// str = str.toLowerCase();
+// str = str.toUpperCase();
+// str = str.toLocaleUpperCase();
+// console.log(str);
+
+// Mutable va Immutable
+// str[1] = "‘";
+// console.log(str);
+
+// console.log(str.indexOf("falksdjfk", 6));
+// console.log(str.lastIndexOf("fajksdfkjas", 24));
+
+// console.log(str.includes("buyuk", 23));
+
+// console.log(str.charCodeAt("s"));
+
+// console.log(str.startsWith(" ", 11));
+// console.log(str.endsWith("davlat!"));
+
+// console.log(str.padStart(55, "."));
+// console.log(str.padEnd(55, "."));
+
+// let trim = "   aisdjf     ";
+// console.log(trim.trimStart());
+// console.log(trim.trimEnd());
+// console.log(trim.trim());
+
+// console.log(str.slice(-20, -11));
+
+// console.log(str.substring(0, -11));
+
+// console.log(str.substr(0, 11));
+
+// let str1 = "O'zbekiston";
+// console.log(str + str1);
+// console.log(str.concat(" ", str1));
+
+// console.log(str.replace("!", "?"));
+// console.log(str.replaceAll("k", "q"));
+
+// for (let i in str) {
+//   console.log(i);
+// }
+
+// for (let e of str) {
+//   console.log(e);
+// }
+
+// let str1 = "string0";
+// let str2 = "string1";
+
+// console.log(str1.localeCompare(str2));
