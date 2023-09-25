f7f1ade041526f634a069e2e1df24723d930b727
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-25 17:31:09 +0500

    Lately commit

---
+++ 17. Scope, Closure/index.js
@@ -0,0 +1,31 @@
+// Scope
+// let a = 5;
+// {
+//   // let a = 1;
+//   console.log(a);
+//   {
+//     console.log(a);
+//   }
+// }
+
+// console.log(a);
+
+// 1. Global Scope
+// 2. Module Scope
+// 3. Block Scope
+// 4. Function Scope
+
+// var name = "John";
+
+// if (true) {
+//   // var name = "aziz";
+//   console.log(name);
+// }
+
+// function func() {
+//   var name = "John";
+//   console.log(name);
+// }
+
+// func();
+// console.log(name);
---
+++ 18. IIFE, NFE/index.js
@@ -0,0 +1,25 @@
+// IIFE => Immadiatly Invoked Function Expression
+
+// var name = "John";
+
+// if (true) {
+//   var name = "Aziz";
+//   console.log(name);
+// }
+
+// (function () {
+//   var name = "Aziz";
+//   console.log(name);
+// })();
+// console.log(name);
+
+// NFE => Named Function Expression
+// let func = function name(n) {
+//   if (n == 1) return 1;
+//   return n * name(n - 1);
+// };
+
+// let factorial = func;
+
+// console.log(factorial(5));
+// // console.log(name);
