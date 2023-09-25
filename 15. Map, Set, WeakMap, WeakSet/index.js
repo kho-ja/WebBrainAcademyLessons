a1cf8300f6ad2737e2c3dff99ded99a61b2a2d10
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-25 16:44:04 +0500

    map, set, weakMap, weakSet

---
+++ 15. Map, Set, WeakMap, WeakSet/index.js
@@ -0,0 +1,54 @@
+// Map
+
+// let key = [12, 34, 5, 6];
+// let map = new Map([
+//   ["name", "John"],
+//   [1, "bir"],
+//   [true, "true"],
+//   [undefined, "undefined"],
+//   [null, "null"],
+//   [2672422365873456n, "bigInt"],
+//   [{ name: "John" }, "object"],
+//   [key, "array"],
+// ]);
+
+// console.log(map.get(1));
+// console.log(map.get(true));
+
+// map.set(key, "array");
+
+// console.log(map.has(key));
+
+// console.log(map.keys());
+// console.log(map.values());
+// console.log(map.entries());
+
+// map.delete(1);
+
+// console.log(map.size);
+
+// console.log(map.clear());
+
+// console.log(map);
+
+// set
+
+// let set = new Set([1, 2, 4, 1, 2, 4, 3, 2, 6, 3, "asdfa", key, {}]);
+
+// console.log(set.has(key));
+// console.log(set.add([12, 34, 5, 6]));
+// console.log(set.delete(key));
+// console.log(set.keys());
+// console.log(set.values());
+// console.log(set.entries());
+// console.log(set.clear());
+// console.log(set.forEach((v) => console.log(v)));
+
+// console.log(set);
+
+// let key = { name: "john" };
+
+// let wmap = new WeakMap([[key, "obj"]]);
+// key = null;
+// console.log(wmap);
+// wmap.delete(key);
