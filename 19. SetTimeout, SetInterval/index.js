9d0d6b2b2fbe1261f154c7e269bbe42254f51419
kho-ja <azimxojayevabdulazizxoja@gmail.com>
2023-09-27 12:22:24 +0500

    setInterval, SetTimeout

---
+++ 19. SetTimeout, SetInterval/index.js
@@ -0,0 +1,21 @@
+// Asinxron hamda sinxron
+// Sinxron
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// Asinxron
+// console.log(1);
+// setTimeout(() => console.log(2), 2000);
+// console.log(3);
+
+// let id = setTimeout(() => console.log(2), 5000);
+
+// console.log(id);
+// clearTimeout(id);
+// let count = 0;
+// let id = setInterval(() => {
+//   console.log(2);
+//   count++;
+//   if (count > 5) clearInterval(id);
+// }, 2000);
+// console.log(typeof id);
